2b867546df079578094a36ae0f80f38882047c8f
Juan Pablo Garcia <juanpablo.garcia@alumni.ubc.ca>
2020-10-22 14:28:41 -0500

    Added a try except block to arg int casting. Added a GET resource, lets you know the optimal number of grades per submission given the number of grades we want one hacker to complete.

--- resources/JudgeMaster.py
+++ resources/JudgeMaster.py
@@ -7,10 +7,26 @@ from judge_master import JudgeMaster
 class JudgeMasterResource(Resource):
     def post(self):
         # grab number of grades per team from args
-        grades_per_project = int(request.args.get('grades_per_project'))
+        try:
+            grades_per_project = int(request.args.get('grades_per_project'))
+        except:
+            return {'status': 'error', 'message': 'argument grades_per_project is not an int'}, 400
 
         if grades_per_project is None:
             return {'status': 'error', 'message': 'no argument grades_per_project given'}, 400
 
         judge_master = JudgeMaster()
         return judge_master.shuffle(grades_per_project)
+
+    def get(self):
+        # get number of grades per hacker
+        try:
+            grades_per_hacker = int(request.args.get('grades_per_hacker'))
+        except:
+            return {'status': 'error', 'message': 'argument grades_per_hacker is not an int'}, 400
+        if not grades_per_hacker:
+            return {'status': 'error', 'message': 'no argument grades_per_hacker given'}, 400
+
+        judge_master = JudgeMaster()
+        grades_per_submission = judge_master.getOptimalGradesPerSubmission(grades_per_hacker)
+        return {'status': 'success', 'data': 'when looking for {} grades per hacker, {} grades per project is the optimal value'.format(grades_per_hacker, grades_per_submission)}, 200
